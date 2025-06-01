65ca51e89304a9f0e0af6b189e12ed763b51c589
FranDAngelo-sheep <erokdrk9961@gmail.com>
2025-06-01 16:14:04 -0300

    Update cuadrados.js

--- cuadrados.js
+++ cuadrados.js
@@ -2,38 +2,34 @@
 ANCHO CUADRADO
 //////////////////////*/
 
-let anchocuadros = 38;
+let anchocuadros = 57;
 
 
 
 class cuadros{
       
-  constructor(pX,pY,direc/*,paleta*/){
+  constructor(pX,pY){
         this.x = pX;
         this.y = pY;
-        this.direc = direc;
-        this.vel = 5;
         this.paleta = miPaleta;
       ;
 
   }
   dibujar(){
     push();
-    translate(50,50);
+    translate(75,75);
     noStroke();
-    let c = random(this.paleta)
-    fill(c);
+    this.colorRandom();
     square(this.x,this.y,anchocuadros);
-    this.movimiento(this.direc);
     pop();
   }
 
-  movimiento(direccion){
-    this.dir = direccion ;
-    if(this.dir == 1){
-      this.x -= 1;
-    } else if(this.dir == 2){
-      this.x += 1;
-    }
+  colorRandom(){
+    
+ 
+    let c = random(this.paleta);
+    fill(c);
+
+ 
   }
 }
