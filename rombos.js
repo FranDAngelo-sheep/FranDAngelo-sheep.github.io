029cdf992295d57633e5328a0ce47af4e1b5a6f7
FranDAngelo-sheep <erokdrk9961@gmail.com>
2025-06-03 22:38:10 -0300

    Update rombos.js

--- rombos.js
+++ rombos.js
@@ -1,25 +1,28 @@
 class rombos{
       
   constructor(pX,pY){
-        this.x = pX;
-        this.y = pY;
-        this.ancho = 30;
-        this.paleta = miPaleta;
-        
+      this.x = pX;
+      this.y = pY;
+      this.ancho = 30;
+      this.cambioColor= map(mouseX,0,width,0,200);
+      this.paleta = [
+      color(this.cambioColor+160, 65, 80),  
+      color(this.cambioColor+120, 65, 50),        
+      color(this.cambioColor+80, 65, 80),  
+      color(this.cambioColor+40, 65, 70)];
+      this.colorFinal = this.colorRandom();
   }
   dibujar(){
     push();
     translate(64,94);
     noStroke();
-    this.colorRandom();
+    fill(random([0,100,this.colorFinal]));
     quad(this.x,this.y,this.x+this.ancho,this.y-this.ancho,this.x+this.ancho*2,this.y,this.x+this.ancho,this.y+this.ancho);
     pop();
   }
 
   colorRandom(){
-    
-    let c = random(this.paleta);
-    fill(random([0,100,c]));
-    
+    var c = random(this.paleta);
+    return c;
   }
 }
